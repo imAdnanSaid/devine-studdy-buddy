75775cf8ce164aaa83c0c5be19df9d9211761623
Adnan Syed <141570307+sleepy-chap@users.noreply.github.com>
2023-10-20 06:32:26 +0500

    Update Allowed Host settings.py

--- Dev/Dev/settings.py
+++ Dev/Dev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_m1#l4-8b42mex70!nk@1%ez^z0!*(_c#qbcs#)q_tdg%u$cba
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
